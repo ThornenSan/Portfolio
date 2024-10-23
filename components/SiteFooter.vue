26834771999ca9cd555d02f0300f8e1e87114f69
San Thornen <58549753+ThornenSan@users.noreply.github.com>
2024-10-24 01:45:50 +0900

    feat: update portfolio

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -8,7 +8,7 @@ const links = [
 <template>
   <footer class="bg-gray-800">
     <section class="mx-auto max-w-7xl px-2 sm:px-4 lg:py-6 text-gray-300 flex flex-row">
-      <p class="flex-1">&copy; Thornen 2023. All Rights Reserved</p>
+      <p class="flex-1">&copy; Thornen 2024. All Rights Reserved</p>
       <div class="flex flex-row justify-center gap-4 mb-4 mt-2">
         <a v-for="link in links" :key="link.name" :href="link.href">
           <Icon :name="link.icon" size="1.5rem" class="text-gray-300" />
--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -26,12 +26,12 @@ const navigation = [
           </DisclosureButton>
         </div>
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-          <div class="flex flex-shrink-0 items-center">
+          <!-- <div class="flex flex-shrink-0 items-center">
             <img class="block h-8 w-auto lg:hidden" src="https://nuxt.com/assets/design-kit/logo/icon-green.png"
               alt="Your Company" />
             <img class="hidden h-8 w-auto lg:block" src="https://nuxt.com/assets/design-kit/logo/icon-green.png"
               alt="Your Company" />
-          </div>
+          </div> -->
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
               <a v-for="item in navigation" :key="item.name" :href="item.href"
